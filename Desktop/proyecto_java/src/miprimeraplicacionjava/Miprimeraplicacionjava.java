27e9a44f47f379ac0b858b112635711a3a6152f9
jennisurio <jennifersurio21@gmail.com>
2022-05-06 14:51:59 -0600

    ejer1

---
+++ Desktop/proyecto_java/src/miprimeraplicacionjava/Miprimeraplicacionjava.java
@@ -0,0 +1,19 @@
+
+package miprimeraplicacionjava;
+
+
+public class Miprimeraplicacionjava {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        string nombreCompleto;
+             
+                
+        String nombrecompleto = "Camila Esperansa Rosales"; 
+    
+         System.out.println("el nombre es " + nombrecompleto);
+       
+    }
+    
+}
